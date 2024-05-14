40c7d1876ac4c6ba3ef14ed3a3c59e00613b1ff4
musman7888 <m.usman7888@gmail.com>
2024-05-14 05:50:44 +0500

    first version

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+// take input from user
+const userInput = await inquirer.prompt([
+    {message: "Please enter paragraph: ",
+    type: "input",
+    name: "paragraph"
+    }
+])
+
+// count the number of word/characters without white spaces
+let wordsArr = userInput.paragraph.trim().split(" ")
+let charArr = userInput.paragraph.trim().replaceAll(" ", "")
+
+
+console.log(`Your entered paragraph has following:
+Number of Words      : ${wordsArr.length}
+Number of Characters : ${charArr.length}`)
